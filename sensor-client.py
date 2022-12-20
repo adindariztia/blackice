5f588e1adc7e0ed4e2a51c0958ce96ea84b752fc
adindariztia <adindariztia@gmail.com>
2022-12-20 19:37:11 +0900

    first commit

---
+++ sensor-client.py
@@ -0,0 +1,24 @@
+import time
+from time import gmtime, strftime
+import board
+import adafruit_dht
+
+
+# initialize the sensor
+dhtdevice = adafruit_dht.DHT11(board.D23)
+
+while True:
+    try:
+        tempCelcius = dhtdevice.temperature
+        humidity = dhtdevice.humidity
+        timestamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    except RuntimeError as error:
+        print(error.args[0])
+        time.sleep(1)
+        continue
+    except Exception as error:
+        print(error.args[0])
+        dhtdevice.exit()
+        raise error
+    
+    time.sleep(1)
