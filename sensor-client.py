90cdc08304d07999a3fea6d79e0753ff18a2b70c
adindariztia <adindariztia@gmail.com>
2022-12-20 20:34:22 +0900

    add simple socket connection

--- sensor-client.py
+++ sensor-client.py
@@ -2,21 +2,38 @@ import time
 from time import gmtime, strftime
 import board
 import adafruit_dht
+import socket
 
 
 # initialize the sensor
 dhtdevice = adafruit_dht.DHT11(board.D23)
+
+#remote server IP address and port
+HOST = "202.31.134.217"
+PORT = 2508
 
 while True:
     try:
         tempCelcius = dhtdevice.temperature
         humidity = dhtdevice.humidity
         timestamp = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        sensor_data = {
+            "timestamp": timestamp,
+            "temperature": tempCelcius,
+            "humidity": humidity
+        }
+        
         print( timestamp +
             " Temp: {:.1f} C    Humidity: {}% ".format(
                 tempCelcius, humidity
             )
         )
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            s.sendall(b"hai adinda")
+            data = s.recv(1024)
+            
+            print("dapet data neh ", data)
     except RuntimeError as error:
         print(error.args[0])
         time.sleep(1)
