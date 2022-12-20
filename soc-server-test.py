fc7ade82cf7b1284cbe386fc2201734c7bec0df3
adindariztia <adindariztia@gmail.com>
2022-12-20 21:50:42 +0900

    fix salah2

--- soc-server-test.py
+++ soc-server-test.py
@@ -1,5 +1,6 @@
 import socket
 import json
+import csv
 
 HOST = "202.31.134.217" 
 PORT = 2508
@@ -15,7 +16,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = conn.recv(1024)
             tmpStr = data.decode()
             jsonObj = json.loads(tmpStr)
-            
+            saveData(jsonObj)
             if not data:
                 break
             conn.send(data)
+            
+def saveData(data):
+    with open('dataset_sensor.csv', 'a', encoding='UTF8') as f:
+        writer =csv.writer(f, delimiter=';')
+        writer.writerow([data["timestamps"], data["temperature"], data["humidity"]])
