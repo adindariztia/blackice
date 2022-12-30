0a7ebf62239b2e317fc0a9cd7a896966206301c5
adindariztia <adindariztia@gmail.com>
2022-12-30 14:11:44 +0900

    tambah file buat save dataset directly abis disense dari sensor

---
+++ sensor_dataset.py
@@ -0,0 +1,54 @@
+###################################################
+####### Temperature & Humidity client sensor ######
+####### Author: Adinda Riztia Putri ###############
+###################################################
+
+
+import time
+from time import gmtime, strftime
+import board
+import adafruit_dht
+import socket
+import json
+import csv
+
+# initialize the sensor
+dhtdevice = adafruit_dht.DHT11(board.D23)
+
+def saveData(data):
+    with open('dht11_dataset_{}.csv'.format(strftime("%Y-%m-%d",gmtime())), 'a', encoding='UTF8') as f:
+        writer =csv.writer(f, delimiter=';')
+        writer.writerow([data["timestamp"], data["temperature"], data["humidity"]])
+
+while True:
+    try:
+        tempCelcius = dhtdevice.temperature #get current temperature reading
+        humidity = dhtdevice.humidity #get current temperature reading
+        timestamp = strftime("%Y-%m-%d %H:%M:%S", gmtime()) #get current time reading
+    
+        sensor_data = {
+            "timestamp": timestamp,
+            "temperature": tempCelcius,
+            "humidity": humidity
+        }
+        
+        #print sensor reading value for log info
+        print( timestamp +
+            " Temp: {:.1f} C    Humidity: {}% ".format(
+                tempCelcius, humidity
+            )
+        )
+        
+        saveData(sensor_data)
+            
+
+    except RuntimeError as error:
+        print(error.args[0])
+        time.sleep(1)
+        continue
+    except Exception as error:
+        print(error.args[0])
+        dhtdevice.exit()
+        raise error
+    
+    time.sleep(1)
